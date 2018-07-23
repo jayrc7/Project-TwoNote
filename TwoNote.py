a3117ee581230f988c5b3b4e549b5d6c2b5d7b76
Jason Cabrera <cabrerajason7@gmail.com>
2018-07-23 00:20:12 -0700

    third commit

--- TwoNote.py
+++ TwoNote.py
@@ -2,7 +2,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 
-#class MainWindow(Gtk.Window): 
+class MainWindow(Gtk.Window): 
    def __init__(self):
       Gtk.Window.__init__(self, title = "TwoNote")
       # size of window
