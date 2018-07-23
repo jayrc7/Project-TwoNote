0050328f863bd48328606ed8c6e41364c8bb235d
Jorge Aparicio <jorge.aparicio@pomona.edu>
2018-07-23 21:47:08 +0000

    Add class and imports

--- file_functions.py
+++ file_functions.py
@@ -1,4 +1,10 @@
-def save():
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class MenuManager:
+    def save():
 
 
 
@@ -31,18 +37,18 @@ def save():
 
 
 
-def open_clicked(self, SimpleAction, parameter):
-        dialog = Gtk.FileChooserDialog("Open an existing file", None,
+    def open_clicked(self, SimpleAction, parameter):
+            dialog = Gtk.FileChooserDialog("Open an existing file", None,
                                        Gtk.FileChooserAction.OPEN,
                                        (Gtk.STOCK_CANCEL,
                                         Gtk.ResponseType.CANCEL,
                                         Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
+            response = dialog.run()
+            if response == Gtk.ResponseType.OK:
             filename = dialog.get_filename()
             dialog.destroy()
           
-        elif response == Gtk.ResponseType.CANCEL:
+            elif response == Gtk.ResponseType.CANCEL:
             print("Cancel clicked")
             dialog.destroy()
 
