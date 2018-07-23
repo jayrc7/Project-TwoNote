5699c9c8ba4c7cdb3c7fa14d93e4e5d555579322
Jorge Aparicio <jorge.aparicio@pomona.edu>
2018-07-23 20:31:10 +0000

    Adds open_clicked function

--- file_functions.py
+++ file_functions.py
@@ -31,5 +31,18 @@ def save():
 
 
 
-def open():
+def open_clicked(self, SimpleAction, parameter):
+        dialog = Gtk.FileChooserDialog("Open an existing file", None,
+                                       Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_CANCEL,
+                                        Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            filename = dialog.get_filename()
+            dialog.destroy()
+          
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+            dialog.destroy()
 
