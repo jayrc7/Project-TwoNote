f4264bd417fc400cac0c9e37bb21bf76da2a33bd
Jorge Aparicio <jorge.aparicio@pomona.edu>
2018-07-24 20:47:56 -0700

    added self to all variables

--- TwoNote.py
+++ TwoNote.py
@@ -3,120 +3,120 @@ import menu_functions as menu_func
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 
-class MainWindow(Gtk.ApplicationWindow): 
+class MainWindow(Gtk.ApplicationWindow):
 	def __init__(self):
 		Gtk.Window.__init__(self, title = "TwoNote")
-		# size of window
+        # size of window
 		self.set_default_size(1920,1080)
-		#self.set_border_width(10)
-	    
-		header = Gtk.HeaderBar()
-		header.set_show_close_button(True)
-		header.props.title = "TwoNote"
-		self.set_titlebar(header)
+
+		self.header = Gtk.HeaderBar()
+		self.header.set_show_close_button(True)
+		self.header.props.title = "TwoNote"
+		self.set_titlebar(self.header)
 		
-		#Actions for Menu Button drop down
-		new_action = Gio.SimpleAction.new("new", None)
-		new_action.connect("activate", self.new_clicked)
-		self.add_action(new_action)
+        #Actions for Menu Button drop down
+		self.new_action = Gio.SimpleAction.new("new", None)
+		self.new_action.connect("activate", self.new_clicked)
+		self.add_action(self.new_action)
 		
-		new_book_action = Gio.SimpleAction.new("new_notebook", None)
-		new_book_action.connect("activate", self.new_book_clicked)
-		self.add_action(new_book_action)
+		self.new_book_action = Gio.SimpleAction.new("new_notebook", None)
+		self.new_book_action.connect("activate", self.new_book_clicked)
+		self.add_action(self.new_book_action)
 		
-		open_action = Gio.SimpleAction.new("open", None)
-		open_action.connect("activate", self.open_clicked)
-		self.add_action(open_action)
+		self.open_action = Gio.SimpleAction.new("open", None)
+		self.open_action.connect("activate", self.open_clicked)
+		self.add_action(self.open_action)
 		
-		save_action = Gio.SimpleAction.new("save", None)
-		save_action.connect("activate", self.save_clicked)
-		self.add_action(save_action)
+		self.save_action = Gio.SimpleAction.new("save", None)
+		self.save_action.connect("activate", self.save_clicked)
+		self.add_action(self.save_action)
 		
-		save_as_action = Gio.SimpleAction.new("save_as", None)
-		save_as_action.connect("activate", self.save_as_clicked)
-		self.add_action(save_as_action)
+		self.save_as_action = Gio.SimpleAction.new("save_as", None)
+		self.save_as_action.connect("activate", self.save_as_clicked)
+		self.add_action(self.save_as_action)
 		
-		settings_action = Gio.SimpleAction.new("settings", None)
-		settings_action.connect("activate", self.settings_clicked)
-		self.add_action(settings_action)
+		self.settings_action = Gio.SimpleAction.new("settings", None)
+		self.settings_action.connect("activate", self.settings_clicked)
+		self.add_action(self.settings_action)
 		
 		# men button on right
-		menuButton = Gtk.MenuButton()
+		self.menuButton = Gtk.MenuButton()
       
 		# drop menu
-		menu = Gio.Menu.new()
+		self.menu = Gio.Menu.new()
 		
 		# Creates items for drop Down Menu
-		new_item = Gio.MenuItem.new('New', 'win.new')
-		new_book_item = Gio.MenuItem.new('New Notebook', 'win.new_notebook')
-		open_item = Gio.MenuItem.new('Open', 'win.open')
-		save_item = Gio.MenuItem.new('Save', 'win.save')
-		save_as_item = Gio.MenuItem.new('Save as', 'win.save_as')
-		settings_item = Gio.MenuItem.new('Preferences', 'win.settings')
+		self.new_item = Gio.MenuItem.new('New', 'win.new')
+		self.new_book_item = Gio.MenuItem.new('New Notebook', 'win.new_notebook')
+		self.open_item = Gio.MenuItem.new('Open', 'win.open')
+		self.save_item = Gio.MenuItem.new('Save', 'win.save')
+		self.save_as_item = Gio.MenuItem.new('Save as', 'win.save_as')
+		self.settings_item = Gio.MenuItem.new('Preferences', 'win.settings')
 		
 		# appends items to drop down menu
-		menu.append_item(new_item)
-		menu.append_item(new_book_item)
-		menu.append_item(open_item)
-		menu.append_item(save_item)
-		menu.append_item(save_as_item)
-		menu.append_item(settings_item)
+		self.menu.append_item(self.new_item)
+		self.menu.append_item(self.new_book_item)
+		self.menu.append_item(self.open_item)
+		self.menu.append_item(self.save_item)
+		self.menu.append_item(self.save_as_item)
+		self.menu.append_item(self.settings_item)
 
-		menuButton.set_menu_model(menu)
+		self.menuButton.set_menu_model(self.menu)
     
-		header.pack_start(menuButton)
+		self.header.pack_start(self.menuButton)
         	
 		## left vbox for notebook name 
-		vboxLeft = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20)
-		vboxLeft.set_homogeneous(False)
+		self.vboxLeft = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20)
+		self.vboxLeft.set_homogeneous(False)
 
 		## frame for left vbox
-		leftFrame = Gtk.Frame()
-		leftFrame.add(vboxLeft)
-		leftFrame.set_hexpand(True)
-		leftFrame.set_vexpand(True)
+		self.leftFrame = Gtk.Frame()
+		self.leftFrame.add(self.vboxLeft)
+		self.leftFrame.set_hexpand(True)
+		self.leftFrame.set_vexpand(True)
 		
 		## right vbox for notes 
-		vboxRight = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20)
-		vboxRight.set_homogeneous(False)
-		container = Gtk.ScrolledWindow()
-		text = Gtk.TextView()
-		text.set_indent(10)
-		text.set_top_margin(20)
-		text.set_left_margin(20)
-		container.add(text)
-		vboxRight.pack_start(container, True, True, 0)
+		self.vboxRight = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20)
+		self.vboxRight.set_homogeneous(False)
+		self.scroll_container = Gtk.ScrolledWindow()
+		self.text = Gtk.TextView()
+		self.text.set_indent(10)
+		self.text.set_top_margin(20)
+		self.text.set_left_margin(20)
+		self.scroll_container.add(self.text)
+		self.vboxRight.pack_start(self.scroll_container, True, True, 0)
 
 		# frame for right box
-		rightFrame = Gtk.Frame()
-		rightFrame.add(vboxRight)
-		rightFrame.set_hexpand(True)
-		rightFrame.set_vexpand(True)
+		self.rightFrame = Gtk.Frame()
+		self.rightFrame.add(self.vboxRight)
+		self.rightFrame.set_hexpand(True)
+		self.rightFrame.set_vexpand(True)
 
 		# creates grid and adds frames
-		grid = Gtk.Grid()
-		grid.attach(leftFrame,0,0,2,10)
-		grid.attach_next_to(rightFrame, leftFrame, Gtk.PositionType.RIGHT, 7,10)
+		self.grid = Gtk.Grid()
+		self.grid.attach(self.leftFrame,0,0,2,10)
+		self.grid.attach_next_to(self.rightFrame, self.leftFrame, Gtk.PositionType.RIGHT, 7,10)
 
-		self.add(grid)
+		self.add(self.grid)
 
 
 		# Create a box of linked items
-		box = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
+		self.box = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
 
 		# says whatever we put in this box, theyre going to be together
-		Gtk.StyleContext.add_class(box.get_style_context(), "linked")
+		Gtk.StyleContext.add_class(self.box.get_style_context(), "linked")
 
 		# left arrow
-		leftArrow = Gtk.Button()
-		leftArrow.add(Gtk.Arrow(Gtk.ArrowType.LEFT, Gtk.ShadowType.NONE))
-		box.add(leftArrow)
+		self.leftArrow = Gtk.Button()
+		self.leftArrow.add(Gtk.Arrow(Gtk.ArrowType.LEFT, Gtk.ShadowType.NONE))
+		self.box.add(self.leftArrow)
       
-		header.pack_start(box)
+		self.header.pack_start(self.box)
 		
 		self.connect('destroy', Gtk.main_quit)
 		self.show_all()
-		
+
+
 	# Functions that call functions for Menu Button from menu_functions.py
 	def open_clicked(self, action, none):
 		menu_func.open_file()
@@ -128,7 +128,7 @@ class MainWindow(Gtk.ApplicationWindow):
 		print("new notebook")
 		
 	def save_clicked(self, action, none):
-		print("save")	
+		menu_func.save_file(self.text)
     
 	def save_as_clicked(self, action, none):
 		print("save as")  
