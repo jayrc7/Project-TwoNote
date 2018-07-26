2ed0f0554de8e4fea3237b4d9fc789d0a0d838b3
Jorge Aparicio <jorge.aparicio@pomona.edu>
2018-07-25 23:00:28 -0700

    separated text view code into separate class file

---
+++ TextSet.py
@@ -0,0 +1,30 @@
+import gi 
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Pango
+
+
+class TextSet(Gtk.TextView):
+	def __init__(self):
+		Gtk.TextView.__init__(self)
+		self.set_vexpand(True)
+		self.set_indent(10)
+		self.set_top_margin(90)
+		self.set_left_margin(20)
+		self.textbuffer = self.get_buffer()
+		self.tag_bold = self.textbuffer.create_tag("bold", weight = Pango.Weight.BOLD)
+		self.tag_italic = self.textbuffer.create_tag("italic", style=Pango.Style.ITALIC)
+		self.tag_underline = self.textbuffer.create_tag("underline", underline=Pango.Underline.SINGLE)
+    
+	def on_button_clicked(self, widget, tagname):
+		if tagname == "Bold":
+			tag= self.tag_bold
+		elif tagname == "Italic":
+			tag = self.tag_italic
+		elif tagname == "Underline":
+			tag = self.tag_underline
+		name = widget.get_icon_name()
+		value = widget.get_active()
+		bounds = self.textbuffer.get_selection_bounds()
+		if len(bounds) != 0:
+			start, end = bounds
+			self.textbuffer.apply_tag(tag, start, end)
