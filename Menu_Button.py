8570b8ed232fbcc6f79586f8485db50f378900e8
Jorge Aparicio <jorge.aparicio@pomona.edu>
2018-07-27 15:23:26 -0700

    fixed menu

--- Menu_Button.py
+++ Menu_Button.py
@@ -3,48 +3,52 @@ import menu_functions as menu_func
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 
-class NoteMenu(Gio.Application):
+class NoteMenu(Gio.Menu):
 	def __init__(self):
-		Gio.Application.__init__(self)
-		self.new_menu = Gio.Menu()
-		self.new_item = Gio.MenuItem.new('New', 'app.new')
-		self.new_book_item = Gio.MenuItem.new('New Notebook', 'app.new_notebook')
-		self.open_item = Gio.MenuItem.new('Open', 'app.open')
-		self.save_item = Gio.MenuItem.new('Save', 'app.save')
-		self.save_as_item = Gio.MenuItem.new('Save as', 'app.save_as')
-		self.settings_item = Gio.MenuItem.new('Preferences', 'app.settings')
+		Gio.Menu.__init__(self)
+		self.new_item = Gio.MenuItem.new('New', 'win.new')
+		self.new_book_item = Gio.MenuItem.new('New Notebook', 'win.new_notebook')
+		self.open_item = Gio.MenuItem.new('Open', 'win.open')
+		self.save_item = Gio.MenuItem.new('Save', 'win.save')
+		self.save_as_item = Gio.MenuItem.new('Save as', 'win.save_as')
+		self.settings_item = Gio.MenuItem.new('Preferences', 'win.settings')
 		
 		# appends items to drop down menu
-		self.new_menu.append_item(self.new_item)
-		self.new_menu.append_item(self.new_book_item)
-		self.new_menu.append_item(self.open_item)
-		self.new_menu.append_item(self.save_item)
-		self.new_menu.append_item(self.save_as_item)
-		self.new_menu.append_item(self.settings_item)
-		
+		self.append_item(self.new_item)
+		self.append_item(self.new_book_item)
+		self.append_item(self.open_item)
+		self.append_item(self.save_item)
+		self.append_item(self.save_as_item)
+		self.append_item(self.settings_item)
+'''	
+class NoteActionGroup(Gio.SimpleActionGroup):
+	def __init__(self):
+		Gio.SimpleActionGroup.__init__(self)
 		self.new_action = Gio.SimpleAction.new("new", None)
 		self.new_action.connect("activate", self.new_clicked)
-		self.add_action(self.new_action)
+		self.insert(self.new_action)
 		
 		self.new_book_action = Gio.SimpleAction.new("new_notebook", None)
 		self.new_book_action.connect("activate", self.new_book_clicked)
-		self.add_action(self.new_book_action)
+		self.insert(self.new_book_action)
 		
 		self.open_action = Gio.SimpleAction.new("open", None)
 		self.open_action.connect("activate", self.open_clicked)
-		self.add_action(self.open_action)
+		self.insert(self.open_action)
 		
 		self.save_action = Gio.SimpleAction.new("save", None)
 		self.save_action.connect("activate", self.save_clicked)
-		self.add_action(self.save_action)
+		self.insert(self.save_action)
 		
 		self.save_as_action = Gio.SimpleAction.new("save_as", None)
 		self.save_as_action.connect("activate", self.save_as_clicked)
-		self.add_action(self.save_as_action)
+		self.insert(self.save_as_action)
 		
 		self.settings_action = Gio.SimpleAction.new("settings", None)
 		self.settings_action.connect("activate", self.settings_clicked)
-		self.add_action(self.settings_action)
+		self.insert(self.settings_action)
+		
+		
 		
 	def open_clicked(self, action, none):
 		menu_func.open_file(self)
@@ -64,6 +68,5 @@ class NoteMenu(Gio.Application):
 	def settings_clicked(self, action, none):
 		print("settings")	
 
-	def getMenu:
-		return self.new_menu		
 			
+'''
--- TwoNote.py
+++ TwoNote.py
@@ -16,53 +16,11 @@ class MainWindow(Gtk.ApplicationWindow):
 		self.header.props.title = "TwoNote"
 		self.set_titlebar(self.header)
 		
-        #Actions for Menu Button drop down
-		self.new_action = Gio.SimpleAction.new("new", None)
-		self.new_action.connect("activate", self.new_clicked)
-		self.add_action(self.new_action)
-		
-		self.new_book_action = Gio.SimpleAction.new("new_notebook", None)
-		self.new_book_action.connect("activate", self.new_book_clicked)
-		self.add_action(self.new_book_action)
-		
-		self.open_action = Gio.SimpleAction.new("open", None)
-		self.open_action.connect("activate", self.open_clicked)
-		self.add_action(self.open_action)
-		
-		self.save_action = Gio.SimpleAction.new("save", None)
-		self.save_action.connect("activate", self.save_clicked)
-		self.add_action(self.save_action)
-		
-		self.save_as_action = Gio.SimpleAction.new("save_as", None)
-		self.save_as_action.connect("activate", self.save_as_clicked)
-		self.add_action(self.save_as_action)
-		
-		self.settings_action = Gio.SimpleAction.new("settings", None)
-		self.settings_action.connect("activate", self.settings_clicked)
-		self.add_action(self.settings_action)
-		
+       
 		# men button on right
 		self.menuButton = Gtk.MenuButton()
-		self.giomenu =  menu_button.NoteMenu()
 		# drop menu
-		self.menu = menu_button.getMenu()
-		
-		# Creates items for drop Down Menu
-		self.new_item = Gio.MenuItem.new('New', 'win.new')
-		self.new_book_item = Gio.MenuItem.new('New Notebook', 'win.new_notebook')
-		self.open_item = Gio.MenuItem.new('Open', 'win.open')
-		self.save_item = Gio.MenuItem.new('Save', 'win.save')
-		self.save_as_item = Gio.MenuItem.new('Save as', 'win.save_as')
-		self.settings_item = Gio.MenuItem.new('Preferences', 'win.settings')
-		
-		# appends items to drop down menu
-		self.menu.append_item(self.new_item)
-		self.menu.append_item(self.new_book_item)
-		self.menu.append_item(self.open_item)
-		self.menu.append_item(self.save_item)
-		self.menu.append_item(self.save_as_item)
-		self.menu.append_item(self.settings_item)
-
+		self.menu = menu_button.NoteMenu()
 		self.menuButton.set_menu_model(self.menu)
     
 		self.header.pack_start(self.menuButton)
@@ -114,11 +72,34 @@ class MainWindow(Gtk.ApplicationWindow):
       
 		self.header.pack_start(self.box)
 		
+		Gio.SimpleActionGroup.__init__(self)
+		self.new_action = Gio.SimpleAction.new("new", None)
+		self.new_action.connect("activate", self.new_clicked)
+		self.add_action(self.new_action)
+		
+		self.new_book_action = Gio.SimpleAction.new("new_notebook", None)
+		self.new_book_action.connect("activate", self.new_book_clicked)
+		self.add_action(self.new_book_action)
+		
+		self.open_action = Gio.SimpleAction.new("open", None)
+		self.open_action.connect("activate", self.open_clicked)
+		self.add_action(self.open_action)
+		
+		self.save_action = Gio.SimpleAction.new("save", None)
+		self.save_action.connect("activate", self.save_clicked)
+		self.add_action(self.save_action)
+		
+		self.save_as_action = Gio.SimpleAction.new("save_as", None)
+		self.save_as_action.connect("activate", self.save_as_clicked)
+		self.add_action(self.save_as_action)
+		
+		self.settings_action = Gio.SimpleAction.new("settings", None)
+		self.settings_action.connect("activate", self.settings_clicked)
+		self.add_action(self.settings_action)
 		self.connect('destroy', Gtk.main_quit)
-		self.show_all()
-
-
-	# Functions that call functions for Menu Button from menu_functions.py
+		self.show_all()  
+		
+		
 	def open_clicked(self, action, none):
 		menu_func.open_file(self)
 	
@@ -135,7 +116,9 @@ class MainWindow(Gtk.ApplicationWindow):
 		print("save as")  
     
 	def settings_clicked(self, action, none):
-		print("settings")	    
+		print("settings")
+		
+		  
 
 	
 
