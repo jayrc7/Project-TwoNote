b7e07625568113c1cba5f4f1fc3127e3f2838df5
Jason Cabrera <cabrerajason7@gmail.com>
2018-07-31 21:24:45 -0700

    changes

--- TextSet.py
+++ TextSet.py
@@ -13,21 +13,20 @@ class TextSet(Gtk.TextView):
                 self.tb = TextBuffer()
                 self.set_buffer(self.tb)
     
-	def on_button_clicked(self, widget, tagname):
-                print(tagname)
+	def on_button_clicked(self, widget, tagname, widget1, widget2):
 		name = widget.get_icon_name()
 		value = widget.get_active()
 		bounds = self.tb.get_selection_bounds()
+
+                #if(value == False and len(bounds) != 0):
+
+
 		#highlighting
                 if(value == True and len(bounds) != 0):
                     start, end = bounds
                     self.tb.apply_tag_by_name(tagname, start, end)
 
                 self.tb.markup(widget, tagname)
-            
-
-                
-                        
 	
 	def mouse_clicked(self, window, event): 
 		self.button_bold.set_active(False)
@@ -41,13 +40,18 @@ class TextBuffer(Gtk.TextBuffer):
         Gtk.TextBuffer.__init__(self)
         self.connect_after('insert-text', self.text_inserted)
         # A list to hold our active tags
-        self.tags_on = []
+        self.taglist_on = []
+        #self.taglist_None = []
+        #self.taglist_Bold = []
+        #self.taglist_Italic = []
+        #self.taglist_Underline = []
         # Our Bold tag.
         self.tag_bold = self.create_tag("Bold", weight=Pango.Weight.BOLD)  
         self.tag_none = self.create_tag("None", weight=Pango.Weight.NORMAL)
         self.tag_italic = self.create_tag("Italic", style=Pango.Style.ITALIC)
 	self.tag_underline = self.create_tag("Underline", underline=Pango.Underline.SINGLE)
         self.tag_name = "None"
+
 
     def get_iter_position(self):
         return self.get_iter_at_mark(self.get_insert())
@@ -58,37 +62,32 @@ class TextBuffer(Gtk.TextBuffer):
         ''' add "bold" to our active tags list '''
         if(widget.get_active() == True):
             if(self.tag_name == 'Bold'):
-                if 'Bold' in self.tags_on:
-                    del self.tags_on[self.tags_on.index('Bold')]
+                if 'Bold' in self.taglist_on:
+                    del self.taglist_on[self.tags_on.index('Bold')]
                 else:
-                    self.tags_on.append('Bold')
+                    self.taglist_on.append('Bold')
+
 
             if(self.tag_name == 'Italic'):
-                if 'Italic' in self.tags_on:
-                    del self.tags_on[self.tags_on.index('Italic')]
+                if 'Italic' in self.taglist_on:
+                    del self.taglist_on[self.tags_on.index('Italic')]
                 else:
-                    self.tags_on.append('Italic')
+                    self.taglist_on.append('Italic')
 
             if(self.tag_name == 'Underline'):
-                if 'Underline' in self.tags_on:
-                    del self.tags_on[self.tags_on.index('Underline')]
+                if 'Underline' in self.taglist_on:
+                    del self.taglist_on[self.tags_on.index('Underline')]
                 else:
-                    self.tags_on.append('Underline')
-
-
-
-            
+                    self.taglist_on.append('Underline')      
 
         else:
             self.check = False
-            self.tags_on.append('None')
-        
-
-            
+            self.taglist_on.append('None')
 
     def text_inserted(self, buffer, iter, text, length):
         # A text was inserted in the buffer. If there are ny tags in self.tags_on,   apply them
-        if self.tags_on:
+        #if self.taglist_None or self.taglist_Italic or self.taglist_Underline or self.taglist_Bold:
+        if self.taglist_on:
             # This sets the iter back N characters
             iter.backward_chars(length)
             # And this applies tag from iter to end of buffer
