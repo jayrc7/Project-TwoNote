36ef37b39a282a7c2a6c6f5a0cdfbb66b049a4e0
Jorge Aparicio <jorge.aparicio@pomona.edu>
2018-08-18 00:04:02 -0700

    Separated most of sidebar functionality into sidebar file comments coming soon to a theater near you

--- sidebar_menu.py
+++ sidebar_menu.py
@@ -1,45 +1,165 @@
 import gi
-gi.require_version('Gtk', '3.0')
+import Notebook as note
+import BinaryTree as tree
 from gi.repository import Gtk, Gio
 
+gi.require_version('Gtk', '3.0')
+
+
 class SidebarWindow(Gtk.Frame):
-	def __init__(self):
-		Gtk.Frame.__init__(self)
+    def __init__(self, textbuffer, win):
+        Gtk.Frame.__init__(self, )
+        self.buff = textbuffer
+        self.popup = None
+        self.win = win
+        # keeps track of notebooks
+        self.notebook_list = []
 
-		hbox = Gtk.Box(spacing=6)
-		self.add(hbox)
+        # keeps track of buttons
+        self.notebook_buttons = []
 
-		listbox = Gtk.ListBox()
-		listbox.set_selection_mode(Gtk.SelectionMode.NONE)
-		hbox.pack_start(listbox, True, True, 0)
+        '''
+        set character limit on page
+        make delete and remove buttons work
+        made thread work activate one at a time
+        '''
 
-		row = Gtk.ListBoxRow()
-		hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-		row.add(hbox)
-		vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-		hbox.pack_start(vbox, True, True, 0)
+        # current notebook instance variable
+        self.notebookname = None
+        self.pagename = None
 
-		label1 = Gtk.Label("Automatic Data & Time", xalign=0)
-		label2 = Gtk.Label("Requires internet access", xalign=0)
-		vbox.pack_start(label1, True, True, 0)
-		vbox.pack_start(label2, True, True, 0)
+        # for binary trees
+        self.notebook = None
+        self.page = None
 
-		listbox.add(row)
+        # for gui
+        self.gui_notebook = None
 
-		row = Gtk.ListBoxRow()
-		hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-		row.add(hbox)
-		label = Gtk.Label("Enable Automatic Update", xalign=0)
-		hbox.pack_start(label, True, True, 0)
-      
-		listbox.add(row)
+        # grid to hold everything on the side menu
+        self.menu_grid = Gtk.Grid()
+        self.menu_grid.set_hexpand(True)
 
-		row = Gtk.ListBoxRow()
-		hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-		row.add(hbox)
-		label = Gtk.Label("Date Format", xalign=0)
-		hbox.pack_start(label, True, True, 0)
-      
-		listbox.add(row)
+        # left vbox for notebook name
+        self.hboxLeft = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        # self.gridLeft = Gtk.Grid()
+        self.hboxLeft.set_homogeneous(False)
+        self.notebook_layout = Gtk.Notebook()
+        self.hboxLeft.set_property("width-request", 20)
+        self.hboxLeft.set_hexpand(True)
 
+        # adds notebook
+        self.notebook_layout.set_tab_pos(Gtk.PositionType.LEFT)
 
+        # sidebar buttons
+        self.rename_button = Gtk.Button(label="R")
+        self.delete_button = Gtk.Button(label="D")
+        self.rename_button.connect("clicked", win.rename)
+        self.delete_button.connect("clicked", win.delete)
+
+        # adds notebook to vbox
+        self.hboxLeft.pack_start(self.notebook_layout, True, True, 0)
+
+        # creates hbox for rename notebook and delete button
+        self.buttons_Left = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.rename_button.set_property("width-request", 20)
+        self.delete_button.set_property("width-request", 20)
+        self.buttons_Left.pack_start(self.rename_button, False, True, 0)
+        self.buttons_Left.pack_start(self.delete_button, False, True, 0)
+
+        # frame for left vbox
+
+        self.leftFrame = Gtk.Frame()
+        self.leftFrame.add(self.hboxLeft)
+        self.leftFrame.set_hexpand(True)
+        self.leftFrame.set_vexpand(True)
+        self.leftFrame.set_property("width-request", 50)
+
+        # adds frame and buttons to side bar
+        self.menu_grid.attach(self.leftFrame, 0, 0, 2, 1)
+        self.menu_grid.attach_next_to(self.buttons_Left, self.leftFrame, Gtk.PositionType.BOTTOM, 2, 2)
+        self.add(self.menu_grid)
+
+    def new_book(self, popup):
+        self.popup = popup
+        client_response = self.popup.entry.get_text()
+        if (not self.contains_notebook(client_response)):
+            # save current work (new notebook will clear textview)
+            self.buff.set_text("")
+            self.notebookname = self.popup.entry.get_text()
+            self.notebook = note.Notebook(self.notebookname)
+            self.pagename = self.popup.entry2.get_text()
+            self.page = tree.BinaryTree.Page(self.pagename)
+            self.notebook.add(self.page)
+            self.notebook_list.append(self.notebook)
+
+            # adds notebook to gui
+            self.notebook.add_notebook_gui(self.notebook_layout, self.notebookname)
+
+            ##makes new page current page
+            self.gui_notebook_page = self.notebook.set_current_section(self.notebook_layout)
+
+            # adds page to gui
+            self.notebook.add_page_gui(self.gui_notebook_page, self.pagename)
+
+            self.notebook_layout.show_all()
+
+        else:
+            self.win.duplicate_false()
+
+    def new_page(self, popup):
+        self.popup = popup
+        client_response = self.popup.entry.get_text()
+        if (not self.notebook.contains_page(client_response)):
+            ##SAVE
+            self.buff.set_text("")
+            self.pagename = self.popup.entry.get_text()
+            self.page = tree.BinaryTree.Page(self.pagename)
+            self.notebook.add(self.page)
+            ## update gui
+            self.gui_notebook_page = self.notebook.get_current_page(self.notebook_layout)
+            self.notebook.add_page_gui(self.gui_notebook_page, self.pagename)
+            self.notebook_layout.show_all()
+        else:
+            self.win.duplicate_true()
+
+    def rename(self, popup):
+        self.rename_pop = popup
+        temp = self.pagename
+        response = self.rename_pop.entry_notebook.get_text()
+        response2 = self.rename_pop.entry_page.get_text()
+
+        note_boolean = True
+        if (self.contains_notebook(response)):
+            note_boolean = False
+            if (response == self.notebookname):
+                note_boolean = True
+            else:
+                self.win.duplicate_false()
+
+        if (self.notebook.contains_page(response2)):
+            self.win.duplicate_true()
+
+        # if(not self.notebook.contains_page(response2) and not self.contains_notebook(response)):
+        if (not self.notebook.contains_page(response2) and note_boolean):
+            # changes name instance variables
+            self.notebookname = self.rename_pop.entry_notebook.get_text()
+            self.pagename = self.rename_pop.entry_page.get_text()
+
+            # updates binary tree name and page name
+            self.notebook.set_name(self.notebookname)
+            self.page.set_name(self.pagename)
+
+            # updates notebook tab gui
+            self.gui_notebook_page = self.notebook.get_current_page(self.notebook_layout)
+            label = Gtk.Label(self.notebookname)
+            self.notebook_layout.set_tab_label(self.gui_notebook_page, label)
+
+            # updates notebook page
+            self.notebook.set_page_name(temp, self.pagename)
+
+    def contains_notebook(self, name):
+        for i in range(len(self.notebook_list)):
+            if (self.notebook_list[i].NotebookName == name):
+                return True
+
+        return False
