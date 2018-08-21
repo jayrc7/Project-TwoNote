4ebb2a0250569e99ec4a17d23b4cffd33cd5cc37
Jorge Aparicio <jorge.aparicio@pomona.edu>
2018-08-20 19:23:59 -0700

    format

--- TwoNote.py
+++ TwoNote.py
@@ -1,199 +1,196 @@
 import gi
-import TextSet as text_set
-import Popup as pop
+
 import Menu_Button as menu_button
+import Popup as pop
+import TextSet as text_set
 import sidebar_menu as sidebar
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 import threading
-import time
+
 
 class MainWindow(Gtk.ApplicationWindow):
-	def __init__(self):
-		Gtk.Window.__init__(self, title = "TwoNote")
-		# size of window
-		self.set_default_size(1920,1080)
-		self.header = Gtk.HeaderBar()
-		self.header.set_show_close_button(True)
-		self.header.props.title = "TwoNote"
-		self.set_titlebar(self.header)
-		self.check = None
-		# men button on right
-		self.menuButton = Gtk.MenuButton()
+    def __init__(self):
+        Gtk.Window.__init__(self, title="TwoNote")
+        # size of window
+        self.set_default_size(1920, 1080)
+        self.header = Gtk.HeaderBar()
+        self.header.set_show_close_button(True)
+        self.header.props.title = "TwoNote"
+        self.set_titlebar(self.header)
+        self.check = None
+        # men button on right
+        self.menuButton = Gtk.MenuButton()
 
-		# drop menu
-		self.menu = menu_button.NoteMenu()
-		self.menuButton.set_menu_model(self.menu)
-    
-		self.header.pack_start(self.menuButton)
-        	
-		#buttons for toolbar
-		self.button_bold = Gtk.ToggleToolButton()
-		self.button_italic = Gtk.ToggleToolButton()
-		self.button_underline = Gtk.ToggleToolButton()
-		
-		
-		
-		## right vbox for notes 
-		self.vboxRight = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20)
-		self.vboxRight.set_homogeneous(False)
-		self.scroll_container = Gtk.ScrolledWindow()
-		self.mytext = text_set.TextSet(self.button_bold, self.button_italic, self.button_underline)
-		self.buff = self.mytext.get_buffer()
-		self.scroll_container.add(self.mytext)
-		self.vboxRight.pack_start(self.scroll_container, True, True, 0)
+        # drop menu
+        self.menu = menu_button.NoteMenu()
+        self.menuButton.set_menu_model(self.menu)
 
-		# frame for right box
-		self.rightFrame = Gtk.Frame()
-		self.rightFrame.add(self.vboxRight)
-		self.rightFrame.set_hexpand(True)
-		self.rightFrame.set_vexpand(True)
-		
-		#Left Side Bar 
-		self.leftFrame = sidebar.SidebarWindow(self.buff, self)
+        self.header.pack_start(self.menuButton)
 
-		# creates grid and adds frames
-		self.grid = Gtk.Grid()
-		self.grid.attach(self.leftFrame,0,0,1,30)
-		self.toolbar = Gtk.Toolbar()
-		self.grid.attach(self.toolbar, 2, 0, 5, 1)
-		self.grid.attach_next_to(self.rightFrame, self.leftFrame, Gtk.PositionType.RIGHT, 7,30)
+        # buttons for toolbar
+        self.button_bold = Gtk.ToggleToolButton()
+        self.button_italic = Gtk.ToggleToolButton()
+        self.button_underline = Gtk.ToggleToolButton()
 
-		self.button_bold.set_icon_name("format-text-bold-symbolic")
-		self.toolbar.insert(self.button_bold, 0)
+        ## right vbox for notes
+        self.vboxRight = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=20)
+        self.vboxRight.set_homogeneous(False)
+        self.scroll_container = Gtk.ScrolledWindow()
+        self.mytext = text_set.TextSet(self.button_bold, self.button_italic, self.button_underline)
+        self.buff = self.mytext.get_buffer()
+        self.scroll_container.add(self.mytext)
+        self.vboxRight.pack_start(self.scroll_container, True, True, 0)
 
-		self.button_italic.set_icon_name("format-text-italic-symbolic")
-		self.toolbar.insert(self.button_italic, 1)
+        # frame for right box
+        self.rightFrame = Gtk.Frame()
+        self.rightFrame.add(self.vboxRight)
+        self.rightFrame.set_hexpand(True)
+        self.rightFrame.set_vexpand(True)
 
-		self.button_underline.set_icon_name("format-text-underline-symbolic")
-		self.toolbar.insert(self.button_underline, 2)
+        # Left Side Bar
+        self.leftFrame = sidebar.SidebarWindow(self.buff, self)
 
-		self.button_bold.connect("toggled", self.mytext.on_button_bold_clicked)
-		self.button_italic.connect("toggled", self.mytext.on_button_italics_clicked)
-		self.button_underline.connect("toggled", self.mytext.on_button_underline_clicked)
+        # creates grid and adds frames
+        self.grid = Gtk.Grid()
+        self.grid.attach(self.leftFrame, 0, 0, 1, 30)
+        self.toolbar = Gtk.Toolbar()
+        self.grid.attach(self.toolbar, 2, 0, 5, 1)
+        self.grid.attach_next_to(self.rightFrame, self.leftFrame, Gtk.PositionType.RIGHT, 7, 30)
 
-		self.add(self.grid)
+        self.button_bold.set_icon_name("format-text-bold-symbolic")
+        self.toolbar.insert(self.button_bold, 0)
 
-		# Create a box of linked items
-		self.box = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
+        self.button_italic.set_icon_name("format-text-italic-symbolic")
+        self.toolbar.insert(self.button_italic, 1)
 
-		# says whatever we put in this box, theyre going to be together
-		Gtk.StyleContext.add_class(self.box.get_style_context(), "linked")
+        self.button_underline.set_icon_name("format-text-underline-symbolic")
+        self.toolbar.insert(self.button_underline, 2)
 
+        self.button_bold.connect("toggled", self.mytext.on_button_bold_clicked)
+        self.button_italic.connect("toggled", self.mytext.on_button_italics_clicked)
+        self.button_underline.connect("toggled", self.mytext.on_button_underline_clicked)
 
-		self.header.pack_start(self.box)
+        self.add(self.grid)
 
-		Gio.SimpleActionGroup.__init__(self)
-		self.new_action = Gio.SimpleAction.new("new", None)
-		self.new_action.connect("activate", self.new_clicked)
-		self.add_action(self.new_action)
+        # Create a box of linked items
+        self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
 
-		self.new_book_action = Gio.SimpleAction.new("new_notebook", None)
-		self.new_book_action.connect("activate", self.new_book_clicked)
-		self.add_action(self.new_book_action)
+        # says whatever we put in this box, theyre going to be together
+        Gtk.StyleContext.add_class(self.box.get_style_context(), "linked")
 
-		self.open_action = Gio.SimpleAction.new("open", None)
-		self.open_action.connect("activate", self.open_clicked)
-		self.add_action(self.open_action)
+        self.header.pack_start(self.box)
 
-		self.save_action = Gio.SimpleAction.new("save", None)
-		self.save_action.connect("activate", self.save_clicked)
-		self.add_action(self.save_action)
+        Gio.SimpleActionGroup.__init__(self)
+        self.new_action = Gio.SimpleAction.new("new", None)
+        self.new_action.connect("activate", self.new_clicked)
+        self.add_action(self.new_action)
 
-		self.save_as_action = Gio.SimpleAction.new("save_as", None)
-		self.save_as_action.connect("activate", self.save_as_clicked)
-		self.add_action(self.save_as_action)
+        self.new_book_action = Gio.SimpleAction.new("new_notebook", None)
+        self.new_book_action.connect("activate", self.new_book_clicked)
+        self.add_action(self.new_book_action)
 
-		self.settings_action = Gio.SimpleAction.new("settings", None)
-		self.settings_action.connect("activate", self.settings_clicked)
-		self.add_action(self.settings_action)
-		self.connect('destroy', Gtk.main_quit)
-		self.show_all()
+        self.open_action = Gio.SimpleAction.new("open", None)
+        self.open_action.connect("activate", self.open_clicked)
+        self.add_action(self.open_action)
 
+        self.save_action = Gio.SimpleAction.new("save", None)
+        self.save_action.connect("activate", self.save_clicked)
+        self.add_action(self.save_action)
 
-		self.thread = threading.Thread(target = self.run, args = ())
-		self.thread.daemon = True
-		self.thread_bool = True
+        self.save_as_action = Gio.SimpleAction.new("save_as", None)
+        self.save_as_action.connect("activate", self.save_as_clicked)
+        self.add_action(self.save_as_action)
 
-		self.thread.start()
+        self.settings_action = Gio.SimpleAction.new("settings", None)
+        self.settings_action.connect("activate", self.settings_clicked)
+        self.add_action(self.settings_action)
+        self.connect('destroy', Gtk.main_quit)
+        self.show_all()
 
-		if(len(self.leftFrame.notebook_list) == 0):
-			self.popup = pop.PopUp(self, False)
-			self.response = self.popup.run()
-			if(self.response == Gtk.ResponseType.OK):
-				self.leftFrame.new_book(self.popup)
-			self.popup.destroy()
+        self.thread = threading.Thread(target=self.run, args=())
+        self.thread.daemon = True
+        self.thread_bool = True
 
-	def open_clicked(self, action, none):
-		menu_button.open_file(self, self.mytext)
+        self.thread.start()
 
-	def new_clicked(self,  action, none):
-		self.check = True
-		self.popup = pop.PopUp(self, True)
-		while (self.check):
-			self.check = False
-			self.response = self.popup.run()
-			if(self.response == Gtk.ResponseType.OK):
-				self.leftFrame.new_page(self.popup)
-		self.popup.destroy()
+        if (len(self.leftFrame.notebook_list) == 0):
+            self.popup = pop.PopUp(self, False)
+            self.response = self.popup.run()
+            if (self.response == Gtk.ResponseType.OK):
+                self.leftFrame.new_book(self.popup)
+            self.popup.destroy()
 
-	def rename(self, signal):
-      self.rename_pop = pop.Rename(self, self.leftFrame.notebookname, self.leftFrame.pagename)
-      self.check = True
-		while (self.check):
-			self.check = False
-			self.response = self.rename_pop.run()
-			if (self.response == Gtk.ResponseType.OK):
-				self.leftFrame.rename(self.rename_pop)
-      self.rename_pop.destroy()
+    def open_clicked(self, action, none):
+        menu_button.open_file(self, self.mytext)
 
-   def delete(self, signal):
-      self.delete_pop = pop.Delete(self, self.leftFrame.notebook, self.leftFrame.notebookname, self.leftFrame.pagename)
-      self.response = self.delete_pop.run()
-      if(self.response == Gtk.ResponseType.OK):
-         self.leftFrame.delete(self.delete_pop)
-         
-      self.delete_pop.destroy()
+    def new_clicked(self, action, none):
+        self.check = True
+        self.popup = pop.PopUp(self, True)
+        while (self.check):
+            self.check = False
+            self.response = self.popup.run()
+            if (self.response == Gtk.ResponseType.OK):
+                self.leftFrame.new_page(self.popup)
+        self.popup.destroy()
+
+    def rename(self, signal):
+        self.rename_pop = pop.Rename(self, self.leftFrame.notebookname, self.leftFrame.pagename)
+        self.check = True
+        while (self.check):
+            self.check = False
+            self.response = self.rename_pop.run()
+            if (self.response == Gtk.ResponseType.OK):
+                self.leftFrame.rename(self.rename_pop)
+                self.rename_pop.destroy()
 
 
-	def new_book_clicked(self, action, none):
-		self.popup = pop.PopUp(self, False)
-		self.check = True
-		while (self.check):
-			self.check = False
-			self.response = self.popup.run()
-			if(self.response == Gtk.ResponseType.OK):
-				self.leftFrame.new_book(self.popup)
-		self.popup.destroy()
+    def delete(self, signal):
+        self.delete_pop = pop.Delete(self, self.leftFrame.notebook, self.leftFrame.notebookname, self.leftFrame.pagename)
+        self.response = self.delete_pop.run()
+        if (self.response == Gtk.ResponseType.OK):
+            self.leftFrame.delete(self.delete_pop)
 
-	def duplicate_true(self):
-		self.duplicate = pop.Duplicate(self, True)
-		self.duplicate.run()
-		self.duplicate.destroy()
-		self.check = True
+        self.delete_pop.destroy()
 
-	def duplicate_false(self):
-		self.duplicate2 = pop.Duplicate(self, False)
-		self.duplicate2.run()
-		self.duplicate2.destroy()
-		self.check = True
+    def new_book_clicked(self, action, none):
+        self.popup = pop.PopUp(self, False)
+        self.check = True
+        while (self.check):
+            self.check = False
+            self.response = self.popup.run()
+            if (self.response == Gtk.ResponseType.OK):
+                self.leftFrame.new_book(self.popup)
+        self.popup.destroy()
 
-	def save_clicked(self, action, none):
-		menu_button.save_file(self, self.mytext)
+    def duplicate_true(self):
+        self.duplicate = pop.Duplicate(self, True)
+        self.duplicate.run()
+        self.duplicate.destroy()
+        self.check = True
 
-	def save_as_clicked(self, action, none):
-		print("save as")
+    def duplicate_false(self):
+        self.duplicate2 = pop.Duplicate(self, False)
+        self.duplicate2.run()
+        self.duplicate2.destroy()
+        self.check = True
 
-	def settings_clicked(self, action, none):
-		print("settings")
+    def save_clicked(self, action, none):
+        menu_button.save_file(self, self.mytext)
 
-	def run(self):
-		while self.thread:
-			print("do domething")
-			self.thread = False
+    def save_as_clicked(self, action, none):
+        print("save as")
 
-	
+    def settings_clicked(self, action, none):
+        print("settings")
+
+    def run(self):
+        while self.thread:
+            print("do domething")
+            self.thread = False
+
+
 if __name__ == '__main__':
-	win = MainWindow()
-	Gtk.main()
-
+    win = MainWindow()
+    Gtk.main()
